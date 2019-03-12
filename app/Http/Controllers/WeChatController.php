7005816ae5cbdd6f87e3d4b46fb8c30cba4b96df
ZhuPiaocheng <zpc_rambo@163.com>
2019-03-12 22:39:18 +0800

    commit all

---
+++ app/Http/Controllers/WeChatController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rambo
+ * Date: 2019/3/11
+ * Time: 22:13
+ */
+
+namespace App\Http\Controllers;
+
+
+use EasyWeChat\Support\Log;
+
+
+class WeChatController extends Controller
+{
+
+    /**
+     * 处理微信的请求消息
+     *
+     * @return string
+     */
+    public function serve()
+    {
+
+        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+
+        $app = app('wechat.official_account');
+
+        $app->server->push(function ($message) {
+
+            return "欢迎关注 overtrue！";
+
+        });
+
+        return $app->server->serve();
+    }
+}
---
+++ app/Http/Controllers/wechat/wechatService.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rambo
+ * Date: 2019/3/11
+ * Time: 22:12
+ */
+
+namespace App\Http\Controllers\wechat;
+
+
+class wechatService
+{
+
+
+}
